ac8a05eea063d84adee778b36d330779a373ddcb
bekkopapa <kiyokuro1@gmail.com>
2023-05-15 23:09:37 +0900

    so

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-// const fs = require('fs');
-// const http = require('http');
-// const https = require('https');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 const express = require('express');
 const app = express();
 const path = require('path');
 require('dotenv').config();
 
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/sohyunsoo.xyz/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/sohyunsoo.xyz/fullchain.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/sohyunsoo.xyz/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/sohyunsoo.xyz/fullchain.pem', 'utf8');
 
-// const credentials = { key: privateKey, cert: certificate };
-// const httpsServer = https.createServer(credentials, app);
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
 
-// const domain = "sohyunsoo.xyz";
-// app.use(function (req, res, next) {
-//     if (!req.secure) {
-//         res.redirect(`https://${domain}${req.url}`);
-//     } else {
-//         next();
-//     }
-// });
+const domain = "sohyunsoo.xyz";
+app.use(function (req, res, next) {
+    if (!req.secure) {
+        res.redirect(`https://${domain}${req.url}`);
+    } else {
+        next();
+    }
+});
 
-// httpsServer.listen(443, () => {
-//   console.log('HTTPS Server running on port 443');
-// });
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
 
-// const httpServer = http.createServer(app);
+const httpServer = http.createServer(app);
 
-// httpServer.listen(80, () => {
-//   console.log('HTTP Server running on port 80 and redirecting to HTTPS');
-// });
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port 80 and redirecting to HTTPS');
+});
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './')));
