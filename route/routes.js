e98829699829b37e7a7d4b3f683da16fbf8c20f5
bekkopapa <kiyokuro1@gmail.com>
2023-06-15 23:14:14 +0900

    sep

--- route/routes.js
+++ route/routes.js
@@ -6,43 +6,43 @@ const router = express.Router();
   });
   
   router.get('/aigallery', (req, res) => {
-    res.sendFile(__dirname + '/aigallery.html');
+    res.sendFile(path.join(__dirname + '/aigallery.html'));
   });
   
-  router.get('/novel', (req, res) => {
-    res.sendFile(__dirname + '/novel/novelHome.html');
-  });
+router.get('/novel', (req, res) => {
+  res.sendFile(path.join(__dirname, '../novel/novelHome.html'));
+});
   
   router.get('/novel/novel_1', (req, res) => {
-    res.sendFile(__dirname + '/novel/novel.html');
+    res.sendFile(path.join(__dirname + '/novel/novel.html'));
   });
   
   router.get('/novel/novel_2', (req, res) => {
-    res.sendFile(__dirname + '/novel/novel_2.html');
+    res.sendFile(path.join(__dirname + '/novel/novel_2.html'));
   });
   
   router.get('/GPTweb', (req, res) => {
-    res.sendFile(__dirname + '/GPTweb/door.html');
+    res.sendFile(path.join(__dirname + '/GPTweb/door.html'));
   });
   
   router.get('/GPTweb/gpt1', (req, res) => {
-    res.sendFile(__dirname + '/GPTweb/gpt1/gpt.html');
+    res.sendFile(path.join(__dirname + '/GPTweb/gpt1/gpt.html'));
   });
   
   router.get('/GPTweb/gpt2', (req, res) => {
-    res.sendFile(__dirname + '/GPTweb/gpt2/gpt2.html');
+    res.sendFile(path.join(__dirname + '/GPTweb/gpt2/gpt2.html'));
   });
   
   router.get('/GPTweb/gpt3', (req, res) => {
-    res.sendFile(__dirname + '/GPTweb/gpt3/gpt3.html');
+    res.sendFile(path.join(__dirname + '/GPTweb/gpt3/gpt3.html'));
   });
   
   router.get('/GPTweb/gpt4', (req, res) => {
-    res.sendFile(__dirname + '/GPTweb/gpt4/gpt4.html');
+    res.sendFile(path.join(__dirname + '/GPTweb/gpt4/gpt4.html'));
   });
   
   router.get('/board', (req, res) => {
-    res.sendFile(__dirname + '/board/board.html');
+    res.sendFile(path.join(__dirname + '/board/board.html'));
   });
 
   module.exports = router;
